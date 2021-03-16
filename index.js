f2121ee483ed9216cbd12914b9fa9805e4ac7666
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2021-03-15 23:48:28 -0700

    new

---
+++ index.js
@@ -0,0 +1,30 @@
+module.connect = ("realtrickz.js")
+ client.listen("listening to localhost:{host&number}")
+  client.waitFor("Lucid_dev");
+  client.connectDomain("lucidev.tk")
+   create.domain("ashleygfx.tk")
+    if client = off
+ then return("domain fuc*ed by others! curse them LOL")
+  client.connect = "godaddy.com";
+   then return("globalString(1;20)"
+);
+client.recaptcha 
+  ignore = true
+
+ var domain
+var ashleygfx
+var Real_trickz
+var Lucid_dev
+var godaddy
+
+end
+ replace ("/index.js")-("/index.lua");
+  lua.connect = ('roblox.api.gfx.')
+  
+  domain_holderId = "Realtrickz_001747491947"
+  
+  website_CNAME = "45253da9-f648-43aa-ac0f-0888b5c33174.repl.co"
+  website_Name = "www.ashleygfx.tk"
+  wait("2H");
+  
+    process.end;
